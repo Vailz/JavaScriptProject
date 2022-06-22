7ece8edd421182a3121cf4a57d6aaaea7b4ea2fc
Vailz <shevchenko.d.p.i20@gmail.com>
2022-06-22 16:31:16 +0300

    10.4

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/sonic')
+
+var heroesRouter = require('./routes/heroes');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/heroes', heroesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Hero = require("../models/hero").Hero
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+                                title: 'Мир Соника',
+                                menu: menu
+                            });
+    })
+
 });
-
-
-/* Страница Соника */
-router.get('/sonic', function(req, res, next) {
-  res.render('hero', {
-      title: "Соник",
-      picture: "images/sonic.png",
-      desc: "Антропоморфный ёж, рожденный со способностью бежать быстрее, чем скорость звука."
-  });
-});
-
-
-/* Страница Наклза */
-router.get('/knuckles', function(req, res, next) {
-  res.render('hero', {
-      title: "Наклз",
-      picture: "images/knuckles.png",
-      desc: "Антропоморфная ехидна и хранитель Мастера изумруда, могущественной древней реликвии."
-  });
-});
-
-/* Страница Тейлза */
-router.get('/tails', function(req, res, next) {
-  res.render('hero', {
-      title: "Тейлз",
-      picture: "images/tails.png",
-      desc: "Антропоморфный лисёнок, родившийся с генетическим отклонением, которое дало ему два хвоста."
-  });
-});
-
-/* Страница Эггмана */
-router.get('/eggman', function(req, res, next) {
-  res.render('hero', {
-      title: "Доктор Эггман",
-      picture: "images/eggman.png",
-      desc: "Человек, гениальный учёный и заклятый враг ежа Соника."
-  });
-});
-
 
 module.exports = router;
