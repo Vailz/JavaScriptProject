95e73bdace3b5da3a03adc3f5d083e5a57d16b7b
Vailz <shevchenko.d.p.i20@gmail.com>
2022-06-22 12:03:06 +0300

    6.3

---
+++ createDB.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data 
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try  {
+        await client.connect();
+        var database = client.db("sonic");
+        database.dropDatabase()
+        database = client.db("sonic");
+        const heroes = database.collection("heroes");
+        const result = await heroes.insertMany(data);
+        console.log(`${result.insertedCount} documents were inserted`);
+    } finally {
+        await client.close();
+    }
+}
+run()
---
+++ data.js
@@ -0,0 +1,27 @@
+var data = [{
+    title: 'Соник',
+    nick: 'sonic',
+    avatar: 'images/sonic.png',
+    desc: 'Антропоморфный ёж, рожденный со способностью бежать быстрее, чем скорость звука.'
+},
+{
+    title: 'Наклз',
+    nick: 'knuckles',
+    avatar: 'images/knuckles.png',
+    desc: 'Антропоморфная ехидна и хранитель Мастера изумруда, могущественной древней реликвии.'
+},
+{
+    title: 'Тейлз',
+    nick: 'tails',
+    avatar: 'images/tails.png',
+    desc: 'Антропоморфный лисёнок, родившийся с генетическим отклонением, которое дало ему два хвоста.'
+},
+{
+    title: 'Доктор Эггман',
+    nick: 'eggman',
+    avatar: 'images/eggman.png',
+    desc: 'Человек, гениальный учёный и заклятый враг ежа Соника.'
+}
+];
+
+module.exports.data = data;
