3da2327314a1019d47284becb504aad5433dfcf5
Vailz <shevchenko.d.p.i20@gmail.com>
2022-06-23 15:27:36 +0300

    11.3

--- app.js
+++ app.js
@@ -26,10 +26,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+var MongoStore = require('connect-mongo');
 app.use(session({
-  secret: "VinniIsHero",
-  cookie:{maxAge:60*1000}
+secret: "ThreeCats",
+cookie:{maxAge:60*1000},
+store: MongoStore.create({mongoUrl: 'mongodb://localhost/sonic'})
 }))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var Hero = require("../models/hero").Hero
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-      res.cookie('greeting', 'Hi!!!').render('index', {
-                              title: 'Express',
+      req.session.greeting = "Hi!!!!"
+      res.render('index', {
+                              title: 'Мир Соника',
                               menu: menu
                           });
       })
