07beabe89fba9424b183b6799d22a6c22c105f37
Vailz <shevchenko.d.p.i20@gmail.com>
2022-06-23 15:21:00 +0300

    11.1-11.2

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/sonic')
+var session = require("express-session")
 
 var heroesRouter = require('./routes/heroes');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -24,8 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.static(path.join(__dirname, 'bower_components')));
+
+app.use(session({
+  secret: "VinniIsHero",
+  cookie:{maxAge:60*1000}
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,14 @@ var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Мир Соника',
-                                menu: menu
-                            });
-    })
-
-});
+      res.cookie('greeting', 'Hi!!!').render('index', {
+                              title: 'Express',
+                              menu: menu
+                          });
+      })
+  });
 
 module.exports = router;
